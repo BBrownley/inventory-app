c9c736f7a490e9ac59de6ae45988fdaf677fc628
BBrownley <brendobrownley@yahoo.com>
2020-02-06 22:45:20 -0500

    updated populatedb file

--- populatedb.js
+++ populatedb.js
@@ -10,11 +10,11 @@ if (!userArgs[0].startsWith('mongodb')) {
 */
 
 const fs = require("fs");
+const path = require("path");
 
 var async = require('async')
 var Category = require('./models/category')
 var Item = require('./models/item')
-var ItemInstance = require('./models/iteminstance')
 var Manufacturer = require('./models/manufacturer')
 
 var mongoose = require('mongoose');
@@ -28,12 +28,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 let categories = [];
 let items = [];
-let iteminstances = [];
 let manufacturers = [];
 
 function manufacturerCreate(name, year_established, imagePath, callback) {
 
-    const manufacturerDetail = { name };
+    const manufacturerDetail = { name, image: { data: null, contentType: null } };
 
     if (year_established) {
         manufacturerDetail.year_established = year_established;
@@ -43,7 +42,7 @@ function manufacturerCreate(name, year_established, imagePath, callback) {
         manufacturerDetail.image.data = fs.readFileSync(imagePath);
         manufacturerDetail.image.contentType = "image/png";
     } else { // Give it a placeholder image instead
-        manufacturerDetail.image.date = fs.readFileSync("express-inventory-application/images/placeholder.png");
+        manufacturerDetail.image.data = fs.readFileSync("express-inventory-application/images/placeholder.png");
         manufacturerDetail.image.contentType = "image/png";
     }
 
@@ -64,10 +63,14 @@ function manufacturerCreate(name, year_established, imagePath, callback) {
 
 function categoryCreate(name, imagePath, callback) {
 
-    const categoryDetail = { name };
+    const categoryDetail = { name, image: { data: null, contentType: null } };
 
     if (imagePath) {
-        manufacturerDetail.image.data = imagePath;
+        categoryDetail.image.data = fs.readFileSync(imagePath);
+        categoryDetail.image.contentType = "image/png";
+    } else {
+        categoryDetail.image.data = fs.readFileSync("express-inventory-application/images/placeholder.png");
+        categoryDetail.image.contentType = "image/png";
     }
 
     const category = new Category(categoryDetail);
@@ -85,12 +88,16 @@ function categoryCreate(name, imagePath, callback) {
 
 }
 
-function itemCreate(category, manufacturer, model, description, price, stock, image, callback) {
+function itemCreate(category, manufacturer, model, description, price, stock, imagePath, callback) {
 
-    const itemDetail = {category, manufacturer, model, description, price, stock};
+    const itemDetail = {category, manufacturer, model, description, price, stock, image: { data: null, contentType: null }};
 
-    if (image) {
-        itemDetail.image = image;
+    if (imagePath) {
+        itemDetail.image.data = fs.readFileSync(imagePath);
+        itemDetail.image.contentType = "image/png";
+    } else {
+        itemDetail.image.data = fs.readFileSync("express-inventory-application/images/placeholder.png");
+        itemDetail.image.contentType = "image/png";
     }
 
     const item = new Item(itemDetail);
@@ -108,35 +115,10 @@ function itemCreate(category, manufacturer, model, description, price, stock, im
 
 }
 
-function iteminstanceCreate(item, status, sold, callback) {
-
-    const iteminstanceDetail = { item, status };
-
-    if (sold) {
-        iteminstanceDetail.sold = sold;
-    }
-
-    const iteminstance = new ItemInstance(iteminstanceDetail);
-
-    iteminstance.save((err) => {
-        if (err) {
-            callback(err, null);
-            return;
-        } else {
-            console.log("New item instance: " + iteminstance);
-            iteminstances.push(iteminstance);
-            callback(null, iteminstance);
-        }
-    })
-
-}
-
-// 
-
 function createCategoriesManufacturers(callback) {
     async.series([
         function(callback) {
-            categoryCreate("Keyboards", "express-inventory-application/images/categoryImages/keyboard.png", callback)
+            categoryCreate("Keyboards", path.resolve(__dirname, "../express-inventory-application/images/categoryImages/keyboard.png"), callback)
         },
         function(callback) {
             categoryCreate("Laptops", "express-inventory-application/images/categoryImages/laptop.png", callback)
@@ -157,92 +139,48 @@ function createCategoriesManufacturers(callback) {
 }
 
 function createItems(callback) {
-    async.series([
+    async.parallel([
         function(callback) {
-            itemCreate(categories[0], manufacturers[0], "Blackwidow", "A Mechanical Keyboard with clicky tactile switches", 4000, 0, "path-to-img", callback)
+            itemCreate(categories[0], manufacturers[0], "Blackwidow", "A Mechanical Keyboard with clicky tactile switches", 4000, 12, "express-inventory-application/images/itemImages/razer-blackwidow.png", callback)
         },
         function(callback) {
-            itemCreate(categories[0], manufacturers[1], "G613", "A wireless mechanical keyboard optimal for both typists and gamers", 6000, 3, "path-to-img", callback)
+            itemCreate(categories[0], manufacturers[1], "G613", "A wireless mechanical keyboard optimal for both typists and gamers", 6000, 3, false, callback)
         },
         function(callback) {
-            itemCreate(categories[0], manufacturers[1], "G513", "OMG RGB :O", 10000, 1, "path-to-img", callback)
+            itemCreate(categories[0], manufacturers[1], "G513", "OMG RGB :O", 10000, 2, false, callback)
         },
         function(callback) {
-            itemCreate(categories[1], manufacturers[0], "Blade 15", "A gaming laptop with... You guessed it... RGB lighting", 140000, 0, false, callback)
+            itemCreate(categories[1], manufacturers[0], "Blade 15", "A gaming laptop with... You guessed it... RGB lighting", 140000, 0, "express-inventory-application/images/itemImages/razer-blade.png", callback)
         },
         function(callback) {
-            itemCreate(categories[2], manufacturers[1], "G933 Artemis", "Wireless gaming headset", 15000, 12, "path-to-img", callback)
+            itemCreate(categories[2], manufacturers[1], "G933 Artemis", "Wireless gaming headset", 15000, 12, false, callback)
         },
         function(callback) {
-            itemCreate(categories[2], manufacturers[2], "Arctis 9X", "Noise cancelling gaming headset that's also wireless", 20000, 45, "path-to-img", callback)
+            itemCreate(categories[2], manufacturers[2], "Arctis 9X", "Noise cancelling gaming headset that's also wireless", 20000, 45, false, callback)
         },
         function(callback) {
-            itemCreate(categories[2], manufacturers[2], "Test item 1", "Test item 1 description", 2000, 5, "path-to-img", callback)
+            itemCreate(categories[2], manufacturers[2], "Test item 1", "Test item 1 description", 2000, 5, false, callback)
         },
         function(callback) {
-            itemCreate(categories[2], manufacturers[2], "Test item 2", "Test item 2 description", 2000, 0, "path-to-img", callback)
+            itemCreate(categories[2], manufacturers[2], "Test item 2", "Test item 2 description", 2000, 0, false, callback)
         }
     ], callback)
 }
 
 // --------------------------------------------------------------------
 
-function createBookInstances(cb) {
-    async.parallel([
-        function(callback) {
-          bookInstanceCreate(books[0], 'London Gollancz, 2014.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[1], ' Gollancz, 2011.', false, 'Loaned', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[2], ' Gollancz, 2015.', false, false, callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Maintenance', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Loaned', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[0], 'Imprint XXX2', false, false, callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[1], 'Imprint XXX3', false, false, callback)
-        }
-        ],
-        // Optional callback
-        cb);
-}
-
-
-
 async.series([
-    createGenreAuthors,
-    createBooks,
-    createBookInstances
+    createCategoriesManufacturers,
+    createItems,
 ],
 // Optional callback
 function(err, results) {
     if (err) {
         console.log('FINAL ERR: '+err);
+    } else {
+        console.log("Results: " + results);
     }
-    else {
-        console.log('BOOKInstances: '+bookinstances);
-        
-    }
+
     // All done, disconnect from database
     mongoose.connection.close();
 });
